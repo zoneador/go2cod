8f3229ac73c3300e4c2dda4bec11a9ae1b23b79a
zoneador <165928909+zoneador@users.noreply.github.com>
2024-04-03 12:52:16 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+﻿// Função para buscar os códigos pelo nome
+function buscar() {
+    var input, filtro, tabela, tr, td, i, txtValue;
+    input = document.getElementById("busca");
+    filtro = input.value.toUpperCase();
+    tabela = document.getElementById("tabela");
+    tr = tabela.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[1]; // Busca pelo segundo td (nome)
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filtro) > -1) {
+                tr[i].style.display = ""; // Mostra a linha se o texto for encontrado
+            } else {
+                tr[i].style.display = "none"; // Oculta a linha se o texto não for encontrado
+            }
+        }
+    }
+}
